3aae843804fc5e188d3c4aab22fb1dc1a7f51057
yoon <cbj0010@naver.com>
2022-12-28 18:43:22 +0900

    [feat]#4 bottomnavi 생성 navi xml 연동

--- app/src/main/java/kr/co/nottodo/presentation/MainActivity.kt
+++ app/src/main/java/kr/co/nottodo/presentation/MainActivity.kt
@@ -1,12 +1,34 @@
 package kr.co.nottodo.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapeAppearanceModel
+import com.google.android.material.shape.TriangleEdgeTreatment
 import kr.co.nottodo.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val radius = resources.getDimension(R.dimen.bottomNaviTopCorner)
+        val shapePathModel = ShapeAppearanceModel.builder()
+            .setTopLeftCorner(CornerFamily.CUT, radius)
+            .setTopRightCorner(CornerFamily.CUT, radius)
+            .setTopEdge(TriangleEdgeTreatment(30.toFloat(), false))
+            .build()
+
+        val backgroundDrawable = MaterialShapeDrawable(shapePathModel).apply {
+            fillColor = ContextCompat.getColorStateList(
+                this@MainActivity,
+                R.color.white
+            ) // change color
+        }
+
+        findViewById<BottomNavigationView>(R.id.custom_bottom).background = backgroundDrawable
     }
 }
