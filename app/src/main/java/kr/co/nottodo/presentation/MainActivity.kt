674fc6b7faf6af565303ac21240601fc64f79a58
yoon <cbj0010@naver.com>
2022-12-31 02:52:02 +0900

    [feat]#4 develop pull

--- app/src/main/java/kr/co/nottodo/presentation/MainActivity.kt
+++ app/src/main/java/kr/co/nottodo/presentation/MainActivity.kt
@@ -3,18 +3,20 @@ package kr.co.nottodo.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.shape.CornerFamily
 import com.google.android.material.shape.MaterialShapeDrawable
 import com.google.android.material.shape.ShapeAppearanceModel
 import com.google.android.material.shape.TriangleEdgeTreatment
 import kr.co.nottodo.R
+import kr.co.nottodo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val radius = resources.getDimension(R.dimen.bottomNaviTopCorner)
         val shapePathModel = ShapeAppearanceModel.builder()
             .setTopLeftCorner(CornerFamily.CUT, radius)
@@ -28,7 +30,7 @@ class MainActivity : AppCompatActivity() {
                 R.color.white
             ) // change color
         }
-
-        findViewById<BottomNavigationView>(R.id.custom_bottom).background = backgroundDrawable
+        binding.customBottom.background = backgroundDrawable
+        binding.customBottom.itemIconTintList = null
     }
 }
