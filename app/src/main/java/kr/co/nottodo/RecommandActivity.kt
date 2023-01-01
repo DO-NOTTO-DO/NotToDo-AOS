6f80bd0754809fba636670a9aabd4e8ce0b3e501
minyoung <alsdud11737@gmail.com>
2023-01-01 17:28:32 +0900

    [feat]#12 recommand view

---
+++ app/src/main/java/kr/co/nottodo/RecommandActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.nottodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecommandActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recommand)
+    }
+}
