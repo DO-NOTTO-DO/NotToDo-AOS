9b9536a94dd96998755894c33a768994f9d51ebf
minyoung <alsdud11737@gmail.com>
2023-01-04 13:13:13 +0900

    [feat]#12 fragment 추천뷰 reyclerview 점검

--- app/src/main/java/kr/co/nottodo/presentation/toplevel/recommendation/RecommendationFragment.kt
+++ app/src/main/java/kr/co/nottodo/presentation/toplevel/recommendation/RecommendationFragment.kt
@@ -7,14 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.NonDisposableHandle.parent
 import kr.co.nottodo.R
 import kr.co.nottodo.databinding.FragmentRecommendationBinding
+import kr.co.nottodo.databinding.ItemRecommendBinding
+import kr.co.nottodo.presentation.toplevel.recommendation.data.Recommendationdata
+
+class RecommendViewHolder(val binding: ItemRecommendBinding) : RecyclerView.ViewHolder(binding.root)
 
 class RecommendationFragment : Fragment() {
 
     private lateinit var binding: FragmentRecommendationBinding
-
     private lateinit var recommendationAdapter: RecommendationAdapter
 
     override fun onCreateView(
@@ -30,8 +34,10 @@ class RecommendationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         // 여기서 프라그먼트 작업을 해주시면 좋습니다
-        recommendationAdapter = RecommendationAdapter()
+        val itemList: List<Recommendationdata> = listOf(Recommendationdata("ㅎㅇ", "ㅎㅇ"), Recommendationdata("ㅎㅇ", "ㅎㅇ") )
+        recommendationAdapter = RecommendationAdapter(itemList)
 
+        binding.rvRecommendation.adapter = recommendationAdapter
         binding.rvRecommendation.apply {
             layoutManager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false)
         }
