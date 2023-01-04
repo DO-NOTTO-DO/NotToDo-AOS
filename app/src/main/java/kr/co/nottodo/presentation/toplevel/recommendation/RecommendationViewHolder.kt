eb84de90e9b86c4a9a67a05bd71babe5aa7eb596
minyoung <alsdud11737@gmail.com>
2023-01-04 13:13:13 +0900

    [feat]#12 fragment 추천뷰 reyclerview 점검

--- app/src/main/java/kr/co/nottodo/presentation/toplevel/recommendation/RecommendationViewHolder.kt
+++ app/src/main/java/kr/co/nottodo/presentation/toplevel/recommendation/RecommendationViewHolder.kt
@@ -2,14 +2,17 @@ package kr.co.nottodo.presentation.toplevel.recommendation
 
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
 import kr.co.nottodo.databinding.ItemRecommendBinding
 import kr.co.nottodo.presentation.toplevel.recommendation.data.Recommendationdata
 
 class RecommendationViewHolder(
-    private val binding : ItemRecommendBinding
+    val binding: ItemRecommendBinding
 ) : RecyclerView.ViewHolder(binding.root) {
-    fun onBind(data:Recommendationdata){
-        binding.ivRecommend.
-    }
 
+    fun onBind(data: Recommendationdata) {
+        Glide.with(binding.root.context)
+            .load("https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory&fname=https://k.kakaocdn.net/dn/EShJF/btquPLT192D/SRxSvXqcWjHRTju3kHcOQK/img.png")
+            .into(binding.ivRecommend)
+    }
 }
