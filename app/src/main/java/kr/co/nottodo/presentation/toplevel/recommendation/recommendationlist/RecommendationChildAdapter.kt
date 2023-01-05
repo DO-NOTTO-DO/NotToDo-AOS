a18c32e990a7ee4f9fca1c800e70f0bd6ee65a69
minyoung <alsdud11737@gmail.com>
2023-01-05 22:24:34 +0900

    [feat]#12 추천뷰 Recommendation Child recyclerview 작업

---
+++ app/src/main/java/kr/co/nottodo/presentation/toplevel/recommendation/recommendationlist/RecommendationChildAdapter.kt
@@ -0,0 +1,38 @@
+package kr.co.nottodo.presentation.toplevel.recommendation.recommendationlist
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.ssong_develop.nestedrecyclerview.viewholder.RecommendationChildViewHolder
+import kr.co.nottodo.R
+import kr.co.nottodo.databinding.ItemNottodoRecommendationListBinding
+import kr.co.nottodo.presentation.toplevel.recommendation.data.RecommendationChildData
+
+class RecommendationChildAdapter : RecyclerView.Adapter<RecommendationChildViewHolder>() {
+
+    private val childItems = mutableListOf<RecommendationChildData>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecommendationChildViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding: ItemNottodoRecommendationListBinding =
+            DataBindingUtil.inflate(
+                layoutInflater,
+                R.layout.item_nottodo_recommendation_list, parent, false)
+        return RecommendationChildViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecommendationChildViewHolder, position: Int) {
+        holder.bind(childItems[position])
+    }
+
+    override fun getItemCount(): Int = childItems.size
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun submitList(list: List<RecommendationChildData>) {
+        childItems.clear()
+        childItems.addAll(list)
+        notifyDataSetChanged()
+    }
+}
