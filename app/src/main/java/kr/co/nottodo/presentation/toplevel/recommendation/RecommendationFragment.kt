d069c2ced88d326af67230a7972e41fdecd3f866
minyoung <alsdud11737@gmail.com>
2023-01-05 22:25:10 +0900

    [feat]#12 추천뷰 Recommendation Fragment 작업

--- app/src/main/java/kr/co/nottodo/presentation/toplevel/recommendation/RecommendationFragment.kt
+++ app/src/main/java/kr/co/nottodo/presentation/toplevel/recommendation/RecommendationFragment.kt
@@ -7,39 +7,53 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.NonDisposableHandle.parent
 import kr.co.nottodo.R
 import kr.co.nottodo.databinding.FragmentRecommendationBinding
-import kr.co.nottodo.databinding.ItemRecommendBinding
-import kr.co.nottodo.presentation.toplevel.recommendation.data.Recommendationdata
+import kr.co.nottodo.presentation.toplevel.recommendation.recommendationlist.RecommendationChildAdapter
+import kr.co.nottodo.presentation.toplevel.recommendation.recommendationlist.RecommendationParentAdapter
 
-class RecommendViewHolder(val binding: ItemRecommendBinding) : RecyclerView.ViewHolder(binding.root)
 
 class RecommendationFragment : Fragment() {
 
-    private lateinit var binding: FragmentRecommendationBinding
+    private var _binding: FragmentRecommendationBinding? = null
+    private val binding : FragmentRecommendationBinding
+        get() = requireNotNull(_binding) {"_binding is Null ..."}
     private lateinit var recommendationAdapter: RecommendationAdapter
+    private lateinit var parentAdapter: RecommendationParentAdapter
+    private lateinit var childAdapter: RecommendationChildAdapter
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate<FragmentRecommendationBinding>(
-            inflater,R.layout.fragment_recommendation, container,false
+        _binding = DataBindingUtil.inflate<FragmentRecommendationBinding>(
+            inflater, R.layout.fragment_recommendation, container, false
         )
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        // 여기서 프라그먼트 작업을 해주시면 좋습니다
-        val itemList: List<Recommendationdata> = listOf(Recommendationdata("ㅎㅇ", "ㅎㅇ"), Recommendationdata("ㅎㅇ", "ㅎㅇ") )
-        recommendationAdapter = RecommendationAdapter(itemList)
+        recommendationAdapter = RecommendationAdapter()
+        parentAdapter = RecommendationParentAdapter()
 
-        binding.rvRecommendation.adapter = recommendationAdapter
         binding.rvRecommendation.apply {
-            layoutManager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false)
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = recommendationAdapter
         }
+
+        binding.rvNottodoRecommendListTitle.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = parentAdapter
+        }
+
+
     }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
 }
