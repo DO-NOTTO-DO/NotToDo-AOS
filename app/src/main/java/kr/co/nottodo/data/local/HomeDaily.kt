2ce6a2b8c2aba8625d104884f1ea2a7a60c6fc94
yoon <cbj0010@naver.com>
2023-01-06 01:45:20 +0900

    [feat]#21 서버 연결 전 서버 연결 전까지 더미 밖으려고 주석처리 중

--- app/src/main/java/kr/co/nottodo/data/local/HomeDaily.kt
+++ app/src/main/java/kr/co/nottodo/data/local/HomeDaily.kt
@@ -1,11 +1,11 @@
 package kr.co.nottodo.data.local
 
 data class HomeDaily(
-    val actions: List<Action>,
-    val completionStatus: String,
-    val goal: String,
+//    val actions: List<Action>,
+//    val completionStatus: String,
+//    val goal: String,
     val id: Int,
-    val situation: String,
+//    val situation: String,
     val title: String
 ) {
     data class Action(
