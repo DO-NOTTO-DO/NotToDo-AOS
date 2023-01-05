934ee451ba0836b95d1df37e5aeadd77cc18ae6e
minyoung <alsdud11737@gmail.com>
2023-01-05 22:23:36 +0900

    [feat]#12 추천뷰 Recommendation recyclerview 작업

--- app/src/main/java/kr/co/nottodo/presentation/toplevel/recommendation/data/RecommendationData.kt
+++ app/src/main/java/kr/co/nottodo/presentation/toplevel/recommendation/data/RecommendationData.kt
@@ -1,6 +1,6 @@
 package kr.co.nottodo.presentation.toplevel.recommendation.data
 
-data class Recommendationdata(
+data class RecommendationData(
     val image: String, // 서버에서 이미지 url이 내려오는 경우 String으로 받아야합니다.
     val name: String,
 )
