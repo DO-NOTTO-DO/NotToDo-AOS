109ffdc3970fa2ce23e15b4e7a5d88465701d940
giovannijunseokim <kingjunseo@gmail.com>
2023-01-05 15:46:27 +0900

    [feat] #16 뷰 페이저 기능 구현 및 탭 레이아웃 연결

---
+++ app/src/main/java/kr/co/nottodo/presentation/achievement/view/AchievementFragment.kt
@@ -0,0 +1,51 @@
+package kr.co.nottodo.presentation.achievement.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
+import kr.co.nottodo.databinding.FragmentAchievementBinding
+import kr.co.nottodo.presentation.achievement.adapter.AchievementViewPagerAdapter
+
+class AchievementFragment : Fragment() {
+    private var _binding: FragmentAchievementBinding? = null
+    private val binding: FragmentAchievementBinding
+        get() = requireNotNull(_binding) { "서치 프래그먼트에서 _binding이 널임" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAchievementBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = AchievementViewPagerAdapter(requireContext())
+        binding.viewpagerAchievement.adapter = adapter
+        binding.viewpagerAchievement.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                adapter.notifyDataSetChanged()
+            }
+        })
+        val tabTitles = listOf<String>("   낫투두 통계 보기   ", "   상황별 통계 보기   ")
+        TabLayoutMediator(
+            binding.tablayoutAchievementStatistics,
+            binding.viewpagerAchievement
+        ) { tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
