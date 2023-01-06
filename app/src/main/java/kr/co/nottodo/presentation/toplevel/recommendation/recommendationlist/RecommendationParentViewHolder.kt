4bb584c7ed9f162ef553ab6633a7ed613ffd8176
minyoung <alsdud11737@gmail.com>
2023-01-07 02:34:40 +0900

    [feat]#12 추천뷰 리사이클러뷰 데이터 받아오는 작업 중

--- app/src/main/java/kr/co/nottodo/presentation/toplevel/recommendation/recommendationlist/RecommendationParentViewHolder.kt
+++ app/src/main/java/kr/co/nottodo/presentation/toplevel/recommendation/recommendationlist/RecommendationParentViewHolder.kt
@@ -1,17 +1,24 @@
 package kr.co.nottodo.presentation.toplevel.recommendation.recommendationlist
 
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kr.co.nottodo.databinding.ItemNottodoRecommendationListTitleBinding
+import kr.co.nottodo.presentation.toplevel.recommendation.data.RecommendationChildData
 import kr.co.nottodo.presentation.toplevel.recommendation.data.RecommendationParentData
 
 class RecommendationParentViewHolder(
-    private val binding: ItemNottodoRecommendationListTitleBinding
+    private val binding: ItemNottodoRecommendationListTitleBinding,
+    private val testChildItemViewClickBlock : (view: View, childData: RecommendationChildData) -> Unit
 ) : RecyclerView.ViewHolder(binding.root) {
 
     private lateinit var parentData: RecommendationParentData
 
-    private val childAdapter: RecommendationChildAdapter = RecommendationChildAdapter()
+    private val childAdapter: ChildAdapter = ChildAdapter(
+        childClickListener = { view, childData ->
+            testChildItemViewClickBlock.invoke(view,childData)
+        }
+    )
 
     init {
         initRecyclerView()
