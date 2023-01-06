97e87b9af0c82b090254b971180a96439a4e56ea
giovannijunseokim <kingjunseo@gmail.com>
2023-01-06 18:29:09 +0900

    [feat]#27 레트로핏 객체 추가

--- app/src/main/java/kr/co/nottodo/data/remote/ApiFactory.kt
+++ app/src/main/java/kr/co/nottodo/data/remote/ApiFactory.kt
@@ -1,7 +1,11 @@
 package kr.co.nottodo.data.remote
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
 
 object ApiFactory {
     private val client by lazy {
@@ -13,4 +17,12 @@ object ApiFactory {
             )
             .build()
     }
+    val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("http://43.201.125.166:3000/api/")
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+
+    inline fun <reified T> create(): T = retrofit.create<T>(T::class.java)
 }
