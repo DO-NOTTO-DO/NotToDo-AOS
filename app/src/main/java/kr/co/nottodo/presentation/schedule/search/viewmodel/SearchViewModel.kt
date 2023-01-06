fa8f58328857a53b500eea3755e5d16614424cf5
giovannijunseokim <kingjunseo@gmail.com>
2023-01-07 04:32:57 +0900

    [feat]#29 낫투두 기록 가져오는 함수 구현

--- app/src/main/java/kr/co/nottodo/presentation/schedule/search/viewmodel/SearchViewModel.kt
+++ app/src/main/java/kr/co/nottodo/presentation/schedule/search/viewmodel/SearchViewModel.kt
@@ -1,17 +1,37 @@
 package kr.co.nottodo.presentation.schedule.search.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import kotlinx.coroutines.launch
+import kr.co.nottodo.data.remote.api.ServicePool
+import kr.co.nottodo.data.remote.model.ResponseHistoryDto
+import retrofit2.await
 
 class SearchViewModel : ViewModel() {
+    private val searchService by lazy { ServicePool.searchService }
+
     val searchBarText: MutableLiveData<String> = MutableLiveData<String>("")
     val isSearchBarTextFilled: LiveData<Boolean> = Transformations.map(searchBarText) {
         it != ""
     }
 
+
     fun setSearchBarText(text: String) {
         searchBarText.value = text
+    }
+
+    private val _getHistoryResult: MutableLiveData<ResponseHistoryDto> = MutableLiveData()
+    val getHistoryResult: LiveData<ResponseHistoryDto>
+        get() = _getHistoryResult
+
+    private val _errorMessage: MutableLiveData<String> = MutableLiveData()
+    val errorMessage: LiveData<String>
+        get() = _errorMessage
+
+    fun getHistory() {
+        viewModelScope.launch {
+            runCatching {
+                searchService.getHistory().await()
+            }.fold({ _getHistoryResult.value = it }, { _errorMessage.value = it.message })
+        }
     }
 }
