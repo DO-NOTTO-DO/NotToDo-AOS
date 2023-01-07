01f70b5d16887e58fb13d80b408f978ae1c7eb4e
giovannijunseokim <kingjunseo@gmail.com>
2023-01-08 04:39:42 +0900

    [feat]#44 MissionDto 작성 완료

---
+++ app/src/main/java/kr/co/nottodo/data/remote/model/MissionDto.kt
@@ -0,0 +1,30 @@
+package kr.co.nottodo.data.remote.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RequestMissionDto(
+    val title: String,
+    val situation: String,
+    val actions: List<String>,
+    val goal: String,
+    val actionDate: String
+)
+
+@Serializable
+data class ResponseMissionDto(
+    val status: Int,
+    val success: Boolean,
+    val message: String,
+    val data: Mission
+) {
+    @Serializable
+    data class Mission(
+        val id: Int,
+        val title: String,
+        val goal: String,
+        val situation: String,
+        val actions: List<String>,
+        val actionDate: String
+    )
+}
