015cfaae36764e56152fb9bf7dd83fe336f72db8
yoon <cbj0010@naver.com>
2023-01-08 05:11:13 +0900

    [feat]#21 Balloon 생성

--- app/src/main/java/kr/co/nottodo/presentation/home/HomeBallonFactory.kt
+++ app/src/main/java/kr/co/nottodo/presentation/home/HomeBallonFactory.kt
@@ -2,26 +2,39 @@ package kr.co.nottodo.presentation.home
 
 import android.content.Context
 import androidx.lifecycle.LifecycleOwner
+import com.skydoves.balloon.ArrowOrientation
 import com.skydoves.balloon.Balloon
 import com.skydoves.balloon.BalloonAnimation
-import com.skydoves.balloon.animations.BalloonRotateDirection
+import com.skydoves.balloon.BalloonSizeSpec
 import kr.co.nottodo.R
 
 class HomeBallonFactory : Balloon.Factory() {
     override fun create(context: Context, lifecycle: LifecycleOwner?): Balloon {
         return Balloon.Builder(context)
-            .setWidthRatio(0.8f)
-            .setHeight(50)
-            .setWidth(400)
-            .setIsVisibleArrow(false)
-            .setCornerRadius(8f)
-            .setAlpha(0.9f)
-            .setTextIsHtml(true)
             .setLayout(R.layout.item_home_ballon)
+//            .setWidthRatio(0.8f)
+            .setHeight(74)
+            .setWidth(144)
+            .setArrowSize(10)
+            .setArrowOrientation(ArrowOrientation.BOTTOM)
+            .setArrowPosition(0.3f)
+            .setCornerRadius(100f)
             .setBackgroundColorResource(R.color.white)
             .setBalloonAnimation(BalloonAnimation.CIRCULAR)
             .setLifecycleOwner(lifecycle)
             .build()
-
     }
+
+    /*
+    .setArrowSize(10)
+            .setArrowOrientation(ArrowOrientation.TOP)
+            .setArrowPosition(0.5f)
+//            .setWidthRatio(0.55f)
+            .setHeight(250)
+            .setCornerRadius(4f)
+            .setBackgroundColorResource(R.color.white)
+            .setBalloonAnimation(BalloonAnimation.CIRCULAR)
+            .setLifecycleOwner(lifecycle)
+            .build()
+     */
 }
