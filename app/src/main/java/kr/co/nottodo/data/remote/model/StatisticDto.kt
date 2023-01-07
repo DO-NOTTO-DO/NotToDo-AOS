46dc2eecedb02b561adb19a5fe35dd5c45750827
giovannijunseokim <kingjunseo@gmail.com>
2023-01-07 16:27:18 +0900

    [feat]#38 상황별 통계 Dto 제작

--- app/src/main/java/kr/co/nottodo/data/remote/model/StatisticDto.kt
+++ app/src/main/java/kr/co/nottodo/data/remote/model/StatisticDto.kt
@@ -1,5 +1,6 @@
 package kr.co.nottodo.data.remote.model
 
+import com.google.android.datatransport.runtime.dagger.multibindings.IntoMap
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -13,5 +14,21 @@ data class ResponseMissionStatisticsDto(
     data class MissionStatistic(
         val count: Int,
         val title: String
+    )
+}
+
+@Serializable
+data class ResponseSituationStatisticsDto(
+    val status: Int,
+    val success: Boolean,
+    val message: String,
+    val data: List<SituationStatistic>
+){
+    @Serializable
+    data class SituationStatistic(
+        val id: Int,
+        val count: Int,
+        val name: Int,
+        val missions: List<ResponseMissionStatisticsDto.MissionStatistic>
     )
 }
