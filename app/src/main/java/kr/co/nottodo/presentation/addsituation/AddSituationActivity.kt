538b1bb1f05a2386a3acd4ba063b00137cac7521
minyoung <alsdud11737@gmail.com>
2023-01-07 19:38:55 +0900

    [feat]#19 상황추가뷰 textwatcher 기능 구현 중

--- app/src/main/java/kr/co/nottodo/presentation/addsituation/AddSituationActivity.kt
+++ app/src/main/java/kr/co/nottodo/presentation/addsituation/AddSituationActivity.kt
@@ -1,12 +1,34 @@
 package kr.co.nottodo.presentation.addsituation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kr.co.nottodo.R
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
+import kr.co.nottodo.databinding.ActivityAddSituationBinding
 
 class AddSituationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddSituationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_situation)
+
+        binding = ActivityAddSituationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+
+    private fun checkEdit() {
+        binding.etAddSituationWriteDirect.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
     }
 }
+
+
+
