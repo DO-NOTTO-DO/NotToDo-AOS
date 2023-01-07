0be74b42e1f7c764aa335bd13faaba731d172ed3
giovannijunseokim <kingjunseo@gmail.com>
2023-01-07 16:32:48 +0900

    [chore]#38 Dto 네이밍 수정

--- app/src/main/java/kr/co/nottodo/data/remote/model/StatisticDto.kt
+++ app/src/main/java/kr/co/nottodo/data/remote/model/StatisticDto.kt
@@ -1,15 +1,14 @@
 package kr.co.nottodo.data.remote.model
 
-import com.google.android.datatransport.runtime.dagger.multibindings.IntoMap
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ResponseMissionStatisticsDto(
+data class ResponseMissionStatisticDto(
     val status: Int,
     val success: Boolean,
     val message: String,
     val data: List<MissionStatistic>
-){
+) {
     @Serializable
     data class MissionStatistic(
         val count: Int,
@@ -18,17 +17,17 @@ data class ResponseMissionStatisticsDto(
 }
 
 @Serializable
-data class ResponseSituationStatisticsDto(
+data class ResponseSituationStatisticDto(
     val status: Int,
     val success: Boolean,
     val message: String,
     val data: List<SituationStatistic>
-){
+) {
     @Serializable
     data class SituationStatistic(
         val id: Int,
         val count: Int,
         val name: Int,
-        val missions: List<ResponseMissionStatisticsDto.MissionStatistic>
+        val missions: List<ResponseMissionStatisticDto.MissionStatistic>
     )
 }
