b78de6ba7711e1855c584a19a9bfb68571134a5c
minyoung <alsdud11737@gmail.com>
2023-01-09 07:50:36 +0900

    [feat]#12 서버통신 responsedto 만드는 중

---
+++ app/src/main/java/kr/co/nottodo/presentation/toplevel/recommendation/data/dto/response/ResponseRecommendationCategorySituationDto.kt
@@ -0,0 +1,17 @@
+package kr.co.nottodo.presentation.toplevel.recommendation.data.dto.response
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseRecommendationCategorySituationDto(
+    val status: Int,
+    val success: Boolean,
+    val message: String,
+    val data: List<ResponseRecommendationCategorySituationDetailDto>,
+) {
+    @Serializable
+    data class ResponseRecommendationCategorySituationDetailDto(
+        val title: String,
+        val action: String,
+    )
+}
