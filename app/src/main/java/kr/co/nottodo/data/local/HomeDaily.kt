3f4f88e7a18d636f25acec365b8fd6f3df1ae782
yoon <cbj0010@naver.com>
2023-01-09 22:36:37 +0900

    [chore]#47 HomeDaily.kt수정

--- app/src/main/java/kr/co/nottodo/data/local/HomeDaily.kt
+++ app/src/main/java/kr/co/nottodo/data/local/HomeDaily.kt
@@ -3,16 +3,24 @@ package kr.co.nottodo.data.local
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class HomeDaily(
-    val id: Int,
-    val title: String,
-    val situation: String,
-    val completionStatus: String,
-    val goal: String,
-    val actions: List<Action>
+data class HomeDailyResponse(
+    val status: Int,
+    val success: Boolean,
+    val message: String,
+    val data: List<HomeDaily>
 ) {
     @Serializable
-    data class Action(
-        val name: String
-    )
+    data class HomeDaily(
+        val id: Int,
+        val title: String,
+        val situation: String,
+        val completionStatus: String,
+        val goal: String,
+        val actions: List<Action> = emptyList()
+    ) {
+        @Serializable
+        data class Action(
+            val name: String
+        )
+    }
 }
