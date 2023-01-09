704516448247b1eedea4ee2038070a8a26efb3d6
yoon <cbj0010@naver.com>
2023-01-09 22:39:16 +0900

    [feat]#47 serializable추가

--- app/src/main/java/kr/co/nottodo/data/remote/RequestHomeMission.kt
+++ app/src/main/java/kr/co/nottodo/data/remote/RequestHomeMission.kt
@@ -1,8 +1,12 @@
 package kr.co.nottodo.data.remote
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class RequestHomeMission(
     val `data`: Data
 ) {
+    @Serializable
     data class Data(
         val completionStatus: String,
         val id: Int
