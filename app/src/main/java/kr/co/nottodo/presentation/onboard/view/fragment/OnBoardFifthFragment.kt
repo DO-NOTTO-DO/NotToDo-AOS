fa108eccf2f22972ca12d3f21c4b0e21f10d1cf1
giovannijunseokim <kingjunseo@gmail.com>
2023-01-10 18:58:50 +0900

    [chore]#61 온보딩 뷰 코드 다이어트

--- app/src/main/java/kr/co/nottodo/presentation/onboard/view/fragment/OnBoardFifthFragment.kt
+++ app/src/main/java/kr/co/nottodo/presentation/onboard/view/fragment/OnBoardFifthFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kr.co.nottodo.databinding.FragmentOnBoardFifthBinding
-import kr.co.nottodo.databinding.FragmentOnBoardSecondBinding
 
 class OnBoardFifthFragment : Fragment() {
 
--- app/src/main/java/kr/co/nottodo/presentation/onboard/view/fragment/OnBoardFourthFragment.kt
+++ app/src/main/java/kr/co/nottodo/presentation/onboard/view/fragment/OnBoardFourthFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kr.co.nottodo.databinding.FragmentOnBoardFourthBinding
-import kr.co.nottodo.databinding.FragmentOnBoardSecondBinding
 
 class OnBoardFourthFragment : Fragment() {
 
