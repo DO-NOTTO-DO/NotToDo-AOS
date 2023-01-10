424e6683af3e40c99cdc40bac85c26803a069dad
yoon <cbj0010@naver.com>
2023-01-11 00:09:40 +0900

    [feat]#47 data class 수정

--- app/src/main/java/kr/co/nottodo/data/remote/model/HomeMissionCheckDto.kt
+++ app/src/main/java/kr/co/nottodo/data/remote/model/HomeMissionCheckDto.kt
@@ -4,14 +4,17 @@ package kr.co.nottodo.data.remote.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class HomeMissionCheckDto(
-    val `data`: CheckTodo,
+data class RequestHomeMissionCheck(val completionStatus: String)
+
+@Serializable
+data class ResponseHomeMissionCheckDto(
     val message: String,
     val status: Int,
-    val success: Boolean
+    val success: Boolean,
+    val `data`: HomeMissionCheckDto
 ) {
     @Serializable
-    data class CheckTodo(
+    data class HomeMissionCheckDto(
         val completionStatus: String,
         val id: Int
     )
