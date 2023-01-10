58087dcbd46840e8bb91e3a2250b2c989399a2ad
yoon <cbj0010@naver.com>
2023-01-11 04:54:23 +0900

    [feat]#71 Home typing에니메이션

--- app/src/main/java/kr/co/nottodo/presentation/home/HomeFragment.kt
+++ app/src/main/java/kr/co/nottodo/presentation/home/HomeFragment.kt
@@ -1,5 +1,7 @@
 package kr.co.nottodo.presentation.home
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -12,6 +14,7 @@ import androidx.fragment.app.viewModels
 import com.skydoves.balloon.Balloon
 import kr.co.nottodo.R
 import kr.co.nottodo.databinding.FragmentHomeBinding
+import kr.co.nottodo.presentation.MainActivity
 import kr.co.nottodo.presentation.schedule.addition.view.AdditionActivity
 import timber.log.Timber
 
@@ -19,13 +22,25 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding ?: error("binding not init")
     private lateinit var outterAdapter: HomeOutterAdapter
+    private val stringBuilder = StringBuilder()
+    private lateinit var lable: String
+    lateinit var mainActivity: MainActivity
+
     private val viewModel by viewModels<HomeFragmentViewModel>()
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        // 2. Context를 Activity로 형변환하여 할당
+        mainActivity = context as MainActivity
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 
@@ -36,6 +51,28 @@ class HomeFragment : Fragment() {
         initStatus()
         showBanner()
     }
+
+    @SuppressLint("SetTextI18n")
+    override fun onStart() {
+        super.onStart()
+        var yoonjeong = true
+        var position = -1
+        Thread {
+            while (yoonjeong) {
+                Thread.sleep(300)
+                mainActivity.runOnUiThread {
+                    if (position < lable.length - 1) {
+                        position += 1
+                        binding.tvHomeMotiveDescription.setText(binding.tvHomeMotiveDescription.text.toString() + lable[position].toString())
+//                        Timber.e(binding.tvHomeMotiveDescription.text.toString())
+                    } else {
+                        yoonjeong = false
+                    }
+                }
+            }
+        }.start()
+    }
+
 
     private fun initAdapter() {
         viewModel.responseCheckResult.observe(viewLifecycleOwner) {
@@ -79,7 +116,6 @@ class HomeFragment : Fragment() {
             Toast.makeText(context, "fail", Toast.LENGTH_SHORT).show()
             viewModel.responseHomeMissionCheck(28, "AMBIGUOUS")
             Timber.e("home1 $it")
-            //todo patch에서 데이터 클래스 observe하고 변했을 경우에 get 서버통신 다시하기
 
             balloon.dismiss()
         }
@@ -95,7 +131,6 @@ class HomeFragment : Fragment() {
     private fun initStatus() {
         viewModel.initServer("2023-01-07")
         viewModel.homeBannerInitServer()
-//        viewModel.homeMissionCheckInitServer(1)
     }
 
     private fun showBanner() {
@@ -106,9 +141,9 @@ class HomeFragment : Fragment() {
                 "이미지3" -> binding.ivHomeNottoGraphic.setImageResource(R.drawable.img_home_graphic3)
                 "이미지4" -> binding.ivHomeNottoGraphic.setImageResource(R.drawable.img_home_graphic4)
             }
-
             it.image
-            binding.tvHomeMotiveDescription.text = it.title
+            lable = it.title
+//            binding.tvHomeMotiveDescription.text = it.title
         }
     }
 
