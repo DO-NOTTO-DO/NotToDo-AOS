b686ba84b3dd4341117f70611f41bbebdb6ba44d
minyoung <alsdud11737@gmail.com>
2023-01-11 01:45:07 +0900

    [feat]#53 상황추가뷰 서버통신 중

---
+++ app/src/main/java/kr/co/nottodo/data/remote/api/AddSituationService.kt
@@ -0,0 +1,9 @@
+package kr.co.nottodo.data.remote.api
+
+import kr.co.nottodo.presentation.toplevel.recommendation.data.responsedto.ResponseAddSituationDto
+import retrofit2.http.GET
+
+interface AddSituationService {
+    @GET("situation")
+    suspend fun get(): ResponseAddSituationDto
+}
