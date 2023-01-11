e7b7d8aa453286e15e79e855881a4dcd70f7c121
giovannijunseokim <kingjunseo@gmail.com>
2023-01-11 22:38:44 +0900

    [feat]#74 스낵바 클래스 생성

---
+++ app/src/main/java/kr/co/nottodo/view/snackbar/CustomSnackBar.kt
@@ -0,0 +1,44 @@
+package kr.co.nottodo.view.snackbar
+
+import android.view.LayoutInflater
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.snackbar.Snackbar
+import kr.co.nottodo.R
+import kr.co.nottodo.databinding.ViewSnackBarBinding
+
+class CustomSnackBar(view: View, private val message: String) {
+    companion object {
+        fun makeSnackBar(view: View, message: String) = CustomSnackBar(view, message)
+    }
+
+    private val context = view.context
+    private val snackbar = Snackbar.make(view, "", 5000)
+    private val snackbarLayout = snackbar.view as Snackbar.SnackbarLayout
+
+    private val inflater = LayoutInflater.from(context)
+    private val snackbarBinding: ViewSnackBarBinding = DataBindingUtil.inflate(inflater, R.layout.view_snack_bar, null, false)
+
+    init {
+        initView()
+        initData()
+    }
+
+    private fun initView() {
+        with(snackbarLayout) {
+            removeAllViews()
+            setPadding(0, 0, 0, 0)
+            setBackgroundColor(ContextCompat.getColor(context, android.R.color.transparent))
+            addView(snackbarBinding.root, 0)
+        }
+    }
+
+    private fun initData() {
+        snackbarBinding.tvSnackBarTitle.text = message
+    }
+
+    fun show() {
+        snackbar.show()
+    }
+}
