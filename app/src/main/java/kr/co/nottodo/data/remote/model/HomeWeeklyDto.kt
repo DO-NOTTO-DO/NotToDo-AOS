5deadbeefe50e1afdae5d259a3668b0a1f33c226
yoon <cbj0010@naver.com>
2023-01-13 20:23:21 +0900

    [feat]#71 변수명 수정

--- app/src/main/java/kr/co/nottodo/data/remote/model/HomeWeeklyDto.kt
+++ app/src/main/java/kr/co/nottodo/data/remote/model/HomeWeeklyDto.kt
@@ -5,14 +5,14 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class HomeWeeklyDto(
+    val `data`: List<Data>,
     val message: String,
     val status: Int,
-    val success: Boolean,
-    val `data`: List<Data>
+    val success: Boolean
 ) {
     @Serializable
     data class Data(
         val actionDate: String,
-        val count: Int
+        val percentage: Double
     )
 }
