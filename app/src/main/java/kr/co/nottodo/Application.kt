e0df7ffaebc1103282a97aab22cfc8969321d437
giovannijunseokim <kingjunseo@gmail.com>
2023-02-06 18:29:00 +0900

    [feat]#104 kakao sdk Application.kt에서 생성

--- app/src/main/java/kr/co/nottodo/Application.kt
+++ app/src/main/java/kr/co/nottodo/Application.kt
@@ -3,14 +3,20 @@ package kr.co.nottodo
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import com.kakao.sdk.common.KakaoSdk
 import timber.log.Timber
 
 class Application : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        setupKakaoSdk()
         setupTimber()
         AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+    }
+
+    private fun setupKakaoSdk() {
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
     }
 
     private fun setupTimber() {
