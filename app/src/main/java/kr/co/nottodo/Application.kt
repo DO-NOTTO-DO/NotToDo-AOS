802508d95682af71f9564ce4e41660ec58729e37
yoon <cbj0010@naver.com>
2023-02-09 03:03:09 +0900

    [feat]#107 Application.kt수정 googleLogin에 필요한 친구 만들기

--- app/src/main/java/kr/co/nottodo/Application.kt
+++ app/src/main/java/kr/co/nottodo/Application.kt
@@ -3,6 +3,9 @@ package kr.co.nottodo
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.kakao.sdk.common.KakaoSdk
 import timber.log.Timber
 
@@ -12,6 +15,7 @@ class Application : Application() {
 
         setupKakaoSdk()
         setupTimber()
+        auth = Firebase.auth
         AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
     }
 
@@ -22,5 +26,19 @@ class Application : Application() {
     private fun setupTimber() {
         if (BuildConfig.DEBUG)
             Timber.plant(Timber.DebugTree())
+    }
+
+    companion object {
+        lateinit var auth: FirebaseAuth
+        var email: String? = null
+        fun checkAuth(): Boolean {
+            var currentUser = auth.currentUser
+            return currentUser?.let {
+                email = currentUser.email
+                currentUser.isEmailVerified
+            } ?: let {
+                false
+            }
+        }
     }
 }
